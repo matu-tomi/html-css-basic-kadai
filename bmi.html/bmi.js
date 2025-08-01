24c3b07aacb1210b2a965faa7431e050bf361cd5
matu-tomi <ykn.tky.tmk.0325@gmail.com>
2025-08-01 12:25:47 +0900

    /bmi.html/bmi.jsを追加

---
+++ bmi.html/bmi.js
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+ <meta charset="UTF-8">
+ <title>JavaScriptの基礎を学ぼう_5章課題</title>
+</head>
+<body>
+ <script src="bmi.js">
+    let userName;
+    userName='体重';
+    let userNumber;
+    userNumber=68;
+    console.log(userName);
+    console.log(userNumber);
+
+    userName='身長';
+    userNumber=1.7;
+    console.log(userName);
+    console.log(userNumber);
+
+    userName='計算式';
+    let bmi=68/(1.7*1.7)
+    console.log(bmi);
+ </script>
+</body>
+</html>
