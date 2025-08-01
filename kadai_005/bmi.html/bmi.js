218b5f3bad23217144d8ccd311bb13cca4d7fdae
matu-tomi <ykn.tky.tmk.0325@gmail.com>
2025-08-01 12:50:28 +0900

    /kadai_005/bmi.html/bmi.jsを追加

---
+++ kadai_005/bmi.html/bmi.js
@@ -0,0 +1,16 @@
+
+    let userName;
+    userName='weight';
+    let userNumber;
+    userNumber=68;
+    console.log(userName);
+    console.log(userNumber);
+
+    userName='height';
+    userNumber=1.7;
+    console.log(userName);
+    console.log(userNumber);
+
+    userName='bmi';
+    let bmi=68/(1.7*1.7)
+    console.log(bmi);
