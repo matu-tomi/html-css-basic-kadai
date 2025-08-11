39c94b8b7c288a5346de4016526ea3c2d1c5d9ac
matu-tomi <ykn.tky.tmk.0325@gmail.com>
2025-08-11 13:39:23 +0900

    /kadai_020/event.html /kadai_020/event.jsを追加

---
+++ kadai_020/event.js
@@ -0,0 +1,6 @@
+const btn=document.getElementById('btn');
+btn.addEventListener('click',()=>{
+    console.log('ボタンをしました');
+});
+const addBtn =document.getElementById('add-btn');
+const parenList =document.getElementById('parent-list');
